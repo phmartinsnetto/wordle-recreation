9e6617653e96f8df08f98263d54d0f00d2e8ba01
phmartinsnetto <82962590+phmartinsnetto@users.noreply.github.com>
2023-01-25 17:23:46 -0300

    Add files via upload

---
+++ src/model/Word.js
@@ -0,0 +1,10 @@
+export function generateRandomNumber() {
+    var today = new Date();
+    var randNum = Number((today.getUTCFullYear().toString() + today.getUTCMonth().toString + today.getUTCDate));
+    return randNum;
+}
+
+ export function retrieveWord(wordList) {
+    
+}
+
