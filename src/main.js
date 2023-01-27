d874bc191d0f5e35dca4cac512bef97f52060371
phmartinsnetto <82962590+phmartinsnetto@users.noreply.github.com>
2023-01-26 23:31:04 -0300

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
-/* document.addEventListener("DOMContentLoaded", function() {
-    // this function runs when the DOM is ready, i.e. when the document has been parsed
-    main();
-    
-}); */
-var data = require('../model/data/wordlist.json');
+//import data from "./data/wordlist.json" assert {type: 'json'};
+
+const data = await fetch('./src/data/wordlist.json')
+    .then(response => response.json());      
 
 function generateRandomNumber() {
     let today = new Date();
-    let randNum = Number((today.getUTCFullYear().toString() + today.getUTCMonth().toString() + today.getUTCDate().toString()));
+    let randNum = Number((today.getUTCDate().toString() + today.getUTCFullYear().toString() + today.getUTCMonth().toString()));
     return randNum;
 }
 
@@ -17,10 +15,23 @@ function retrieveWord(wordList) {
     return wordList[randNum];
 }
 
+function handleButtonClick(id) {
+    if (id == "enter") {
+        console.log("you pressed enter");
+    } else if (id == "backspace") {
+        console.log('you pressed backspace');
+    } else {
+        console.log('you pressed ' + id);
+    }
+}
+
 function main() {
+    console.log(data);
     var targetWord = retrieveWord(data);
     console.log(targetWord);
+    document.querySelector(".key-btn").addEventListener("click", handleButtonClick(e.target.id));
 
 }
 
-main();
+// this function runs when the DOM is ready, i.e. when the document has been parsed
+document.addEventListener('load', main());
